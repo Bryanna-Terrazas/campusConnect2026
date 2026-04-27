eb5275df915633bb939c74f5b445157a379de251
Bryanna-Terrazas <bryanna.terrazas01@utrgv.edu>
2026-04-27 15:11:49 -0500

    Fixing more stuff

--- campusConnectApp/apps.py
+++ campusConnectApp/apps.py
@@ -2,6 +2,7 @@ from django.apps import AppConfig
 
 
 class CampusconnectappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'campusConnectApp'
 
     def ready(self):
--- campusConnectApp/forms.py
+++ campusConnectApp/forms.py
@@ -1,19 +1,45 @@
 from django import forms
 from django.contrib.auth.models import User
-from .models import Profile
-from .models import Posts
+from .models import Profile, Posts
+
 
 class UserUpdateForm(forms.ModelForm):
-	class Meta:
-		model = User
-		fields = ['first_name', 'last_name', 'email']
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        for field in self.fields.values():
+            field.widget.attrs.update({
+                'class': 'form-control'
+            })
+
 
 class ProfileUpdateForm(forms.ModelForm):
-	class Meta:
-		model = Profile
-		fields = ['bio', 'age', 'phone']
+    class Meta:
+        model = Profile
+        fields = ['bio', 'age', 'phone']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        for field in self.fields.values():
+            field.widget.attrs.update({
+                'class': 'form-control'
+            })
+
 
 class PostingToFeed(forms.ModelForm):
-	class Meta:
-		model = Posts
-		fields = ['subject', 'body']
+    class Meta:
+        model = Posts
+        fields = ['subject', 'body']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        for field in self.fields.values():
+            field.widget.attrs.update({
+                'class': 'form-control'
+            })
